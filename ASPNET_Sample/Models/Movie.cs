7c8fa1b186709c730486a2a4eb01e83e1e1e29aa
k.kondo <hahaha.kazusa@gmai.com>
2024-02-16 00:40:04 +0900

    Model修正

--- ASPNET_Sample/Models/Movie.cs
+++ ASPNET_Sample/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ASPNET_Sample.Models
 {
@@ -7,9 +8,12 @@ namespace ASPNET_Sample.Models
         public int Id { get; set; }
         public string? Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
