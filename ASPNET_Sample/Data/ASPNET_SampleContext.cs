44319dfde011db7a2eb4520f0995e83de50ac612
k.kondo <hahaha.kazusa@gmai.com>
2024-02-15 23:25:24 +0900

    プロジェクト初回コミット

---
+++ ASPNET_Sample/Data/ASPNET_SampleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ASPNET_Sample.Models;
+
+namespace ASPNET_Sample.Data
+{
+    public class ASPNET_SampleContext : DbContext
+    {
+        public ASPNET_SampleContext (DbContextOptions<ASPNET_SampleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASPNET_Sample.Models.Movie> Movie { get; set; } = default!;
+    }
+}
