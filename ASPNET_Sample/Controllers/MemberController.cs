14d1888450d61db8189e1d40ac61e8535561d3c6
k.kondo <hahaha.kazusa@gmai.com>
2024-02-17 21:15:25 +0900

    ログイン処理(ハードコーディング)とログイン後の画面遷移を追加

---
+++ ASPNET_Sample/Controllers/MemberController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET_Sample.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
