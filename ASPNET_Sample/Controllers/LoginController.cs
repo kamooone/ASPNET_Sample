adfad207ace101bb2353c76f2bfefde9fcbf5c17
k.kondo <hahaha.kazusa@gmai.com>
2024-02-18 22:31:32 +0900

    ハッシュ化したパスワードを照合して行うログイン処理に修正

--- ASPNET_Sample/Controllers/LoginController.cs
+++ ASPNET_Sample/Controllers/LoginController.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace ASPNET_Sample.Controllers
 {
@@ -30,8 +32,9 @@ namespace ASPNET_Sample.Controllers
         [HttpPost]
         public async Task<IActionResult> Index(LoginViewModel model)
         {
-            var user = await _context.User.FirstOrDefaultAsync(u => u.Username == model.UserName && u.PasswordHash == model.Password);
-            if (user != null)
+            var user = await _context.User.FirstOrDefaultAsync(u => u.Username == model.UserName);
+
+            if (user != null && PasswordMatches(model.Password, user.PasswordHash))
             {
                 _logger.LogInformation("ログインボタンを押してログイン成功");
                 return RedirectToAction("Index", "Member");
@@ -43,6 +46,28 @@ namespace ASPNET_Sample.Controllers
                 return View(model);
             }
         }
+
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                // パスワードをバイト配列に変換してハッシュ化
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                // ハッシュ化されたバイト配列をBase64文字列に変換して返す
+                return Convert.ToBase64String(hashedBytes);
+            }
+        }
+
+        private bool PasswordMatches(string inputPassword, string hashedPassword)
+        {
+            // 入力されたパスワードをハッシュ化
+            string hashedInputPassword = HashPassword(inputPassword);
+
+            // ハッシュ化されたパスワードが一致するかどうかを確認
+            return hashedPassword == hashedInputPassword;
+        }
+
 
         [HttpPost]
         public async Task<IActionResult> Logout()
